622a23b0f4527d6284f0c8a36c9461b6007a4cce
Ismam Hasan Ovi <ismamhasanovi@gmail.com>
2024-12-17 14:05:32 +0600

    17/12/2024 | 02:05 PM

--- Codeforces/APlusBAgain.java
+++ Codeforces/APlusBAgain.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
                                     /*##########################################################
                                     ############################################################
                                     ###                                                      ###
@@ -23,29 +22,3 @@ public class APlusBAgain {
         input.close();
     }
 }
-=======
-                                    /*##########################################################
-                                    ############################################################
-                                    ###                                                      ###
-                                    ###             Author: ISMAM HASAN OVI                  ###
-                                    ###            Created: 07-08-2024 | 19:13:57            ###
-                                    ###                                                      ###
-                                    ############################################################
-                                    ##########################################################*/
-import java.util.Scanner;
-
-public class APlusBAgain {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-
-        int t = input.nextInt();  //test case
-        for(int i=0; i<t; i++){
-            int n = input.nextInt();
-            int lastDigit = n % 10;
-            int firstDigit = (n - lastDigit) / 10;
-            System.out.println((firstDigit+lastDigit));
-        }
-        input.close();
-    }
-}
->>>>>>> cf62158 (13/12/2024 | 05:40 PM)
